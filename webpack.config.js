f27c2827bffd52b67fcb92008f5e8bd0926bef3b
cheetax <dmitriy.grebenev@gmail.com>
2018-08-16 23:30:16 +0300

    new

--- webpack.config.js
+++ webpack.config.js
@@ -37,7 +37,7 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['react', 'es2015'],
+            presets: ['react', 'es2015', 'stage-2'],
           }
         }
       },
